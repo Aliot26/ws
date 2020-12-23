51069595919264589cccc657bd8fa08b03a4f673
Aliot26 <olgakochanowa@gmail.com>
2020-12-23 08:56:11 +0100

    added MediaType and Jackson dataformat xml

--- src/main/java/com/spring/webservice/controller/UserController.java
+++ src/main/java/com/spring/webservice/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.spring.webservice.controller;
 
+import com.spring.webservice.model.UserRest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 /*
@@ -9,14 +11,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users")
 public class UserController {
     @GetMapping
-    public String getUsers(@RequestParam(value = "page",  defaultValue = "1") int page,
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
                            @RequestParam(value = "limit", defaultValue = "20") int limit,
                            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
         return "return user form page " + page + " with limit " + limit + " sort " + sort;
     }
 
-    @GetMapping("/{userId}")
-    public String getUser(@PathVariable String userId) {
-        return "return user by userId " + userId;
+    @GetMapping(value = "/{userId}", produces = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE})
+    public UserRest getUser(@PathVariable String userId) {
+        UserRest user = new UserRest();
+        user.setFirstName("Ivan");
+        user.setLastName("Petroff");
+        user.setEmail("test@test.com");
+
+        System.out.println(user);
+        System.out.println(user.getEmail());
+        System.out.println(user.getUserId());
+        return user;
     }
 }
