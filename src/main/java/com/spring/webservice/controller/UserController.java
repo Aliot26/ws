df1c0ae1ea0278ce69bdff78e84df465996096f7
Aliot26 <olgakochanowa@gmail.com>
2020-12-23 11:26:17 +0100

    added validation's annotations

--- src/main/java/com/spring/webservice/controller/UserController.java
+++ src/main/java/com/spring/webservice/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 /*
  *Created by olga on 22.12.2020
  */
@@ -29,15 +31,26 @@ public class UserController {
         user.setLastName("Petroff");
         user.setEmail("test@test.com");
 
+        return new ResponseEntity<UserRest>(user, HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = {
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_JSON_VALUE},
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequest userDetails) {
+
+        UserRest user = new UserRest();
+        user.setFirstName(userDetails.getFirstName());
+        user.setLastName(userDetails.getLastName());
+        user.setEmail(userDetails.getEmail());
+
+
         System.out.println(user);
         System.out.println(user.getEmail());
         System.out.println(user.getUserId());
         return new ResponseEntity<UserRest>(user, HttpStatus.OK);
-    }
-
-    @PostMapping
-    public String createUser(@RequestBody UserDetailsRequest userDetails){
-
-        return "user is created";
     }
 }
--- src/main/java/com/spring/webservice/model/request/UserDetailsRequest.java
+++ src/main/java/com/spring/webservice/model/request/UserDetailsRequest.java
@@ -1,12 +1,25 @@
 package com.spring.webservice.model.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /*
  *Created by olga on 23.12.2020
  */
 public class UserDetailsRequest {
+    @NotNull(message = "firstName can't be null")
     private String firstName;
+
+    @NotNull(message = "lastName can't be null")
     private String lastName;
+
+    @NotNull(message = "email can't be null")
+    @Email
     private String email;
+
+    @NotNull(message = "firstName can't be null")
+    @Size(min = 3, max = 5, message = "Password must be equal or more than 3 or less than 5")
     private String password;
 
     public String getFirstName() {
